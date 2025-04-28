ccce4b5ccee062783c00f655641fbe0598da3857
Julio Neri <160162554+julioneri@users.noreply.github.com>
2025-04-28 18:41:05 -0300

    add exercise

---
+++ exemplo-pacotes/exemplo-interface/src/estabelecimento/Fabrica.java
@@ -0,0 +1,24 @@
+package estabelecimento;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+import equipamentos.multifuncional.EquipamentoMultifuncional;
+
+public class Fabrica {
+    public static void main(String[] args) {
+        EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+         
+        Impressora impressora = em;
+        Digitalizadora digitalizadora = em;
+        Copiadora copiadora = em;
+
+        impressora.imprimir();
+        digitalizadora.digitalizar();
+        copiadora.copiar();
+
+        em.imprimir();
+        em.digitalizar();
+        em.copiar();
+    }
+}
