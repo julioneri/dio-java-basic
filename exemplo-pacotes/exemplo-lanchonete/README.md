a9716f7e6eb404ee01bf7c2ceabdf38bbb048876
Julio Neri <160162554+julioneri@users.noreply.github.com>
2025-04-28 18:49:21 -0300

    feat: add basic lanchonete simulation

---
+++ exemplo-pacotes/exemplo-lanchonete/README.md
@@ -0,0 +1,14 @@
+## Projeto Java - Lanchonete
+Este projeto simula operações básicas de uma lanchonete usando conceitos de programação orientada a objetos em Java.
+
+## Estrutura de Pacotes
+- lanchonete.area.cliente: ações do cliente.
+
+- lanchonete.atendimento: atividades dos atendentes.
+
+- lanchonete.atendimento.cozinha: rotinas internas da cozinha.
+
+# Observações
+- Foco em práticas básicas de modelagem de classes e métodos.
+
+- Simulação didática sem integração real com banco de dados ou interface gráfica.
---
+++ exemplo-pacotes/exemplo-lanchonete/src/lanchonete/Estabelecimento.java
@@ -0,0 +1,25 @@
+package lanchonete;
+
+import lanchonete.area.cliente.Cliente;
+import lanchonete.atendimento.Atendente;
+import lanchonete.atendimento.cozinha.Cozinheiro;
+
+public class Estabelecimento {
+    public static void main(String[] args) {
+        Cozinheiro cozinheiro = new Cozinheiro();
+
+        //ações que o estabelecimento precisa ter ciência
+        cozinheiro.adicionarComboNoBalcao();
+        cozinheiro.adicionarLancheNoBalcao();
+        cozinheiro.adicionarComboNoBalcao();
+
+        Atendente atendente = new Atendente();
+        atendente.servindoMesa();
+        atendente.receberPagamento();
+
+        Cliente cliente = new Cliente();
+        cliente.escolherLanche();
+        cliente.fazerPedido();
+        cliente.pagarConta();
+    }
+}
